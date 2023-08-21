229cd9fbd56911b24e53d90297ca4fa727f16dde
Matt Megarry <mattmegarry@gmail.com>
2023-08-21 12:59:24 +0100

    [refactor] - code is cleaner

--- config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    USE_WANDB = False
-    TRAIN_EXAMPLES = 2000000
--- sp_test.py
+++
@@ -1,9 +0,0 @@
-import torch
-from sentencepeice_tokenizer import SentencePieceTokenizer
-
-tokenizer = SentencePieceTokenizer()
-
-for i in range(10000):
-  print(i)
-  x = torch.tensor([i])
-  print(tokenizer.decode(x.tolist()))
