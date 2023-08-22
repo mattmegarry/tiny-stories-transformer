9160e8149b75e5d05490252e7c3810c85d268d32
Matt Megarry <mattmegarry@gmail.com>
2023-08-22 11:02:09 +0100

    [feature] - transformer block

--- model.py
+++ model.py
@@ -13,11 +13,8 @@ class DecoderModel(torch.nn.Module):
 
     self.embedding    = torch.nn.Embedding(self.vocab_len, self.embedding_dimensions)
     self.pos_emb      = self.get_pos_matrix()
-
-    self.attn = MultiHeadAttention(self.max_seq_len, self.embedding_dimensions, 4)
-    self.add_and_norm = AddAndNorm(self.embedding_dimensions)
-
-    self.feed_forward = FeedForward(self.embedding_dimensions)
+    self.blocks = Block(self.max_seq_len, self.embedding_dimensions, 4)
+    self.final_layer_norm = torch.nn.LayerNorm(self.embedding_dimensions)
     self.map_to_vocab = torch.nn.Linear(self.embedding_dimensions, self.vocab_len)
 
   def forward(self, x):
@@ -25,11 +22,9 @@ class DecoderModel(torch.nn.Module):
     pos = self.pos_emb[0:x.shape[1], :]
     pos_emb_x = emb + pos
 
-    attn = self.attn(pos_emb_x, x=x)
-    attn_add_norm = self.add_and_norm(attn, pos_emb_x)
-    
-    res = self.feed_forward(attn_add_norm)
-    out = self.map_to_vocab(res)
+    out = self.blocks(pos_emb_x)
+    out = self.final_layer_norm(out)
+    out = self.map_to_vocab(out)
 
     return out
 
@@ -61,7 +56,7 @@ class MultiHeadAttention(torch.nn.Module):
       self.qkv_projection = torch.nn.Linear(embedding_dimensions, 3 * embedding_dimensions)
       self.output_projection = torch.nn.Linear(embedding_dimensions, embedding_dimensions)
 
-   def forward(self, x_embeddings, x):
+   def forward(self, x_embeddings):
       batch_size, seq_len, _ = x_embeddings.size()
 
       q, k, v  = self.qkv_projection(x_embeddings).split(self.embedding_dimensions, dim=2)
@@ -78,7 +73,23 @@ class MultiHeadAttention(torch.nn.Module):
       output = self.output_projection(y)
       return output
 
+class Block(torch.nn.Module):
+    def __init__(self, max_seq_len, embedding_dimensions, num_heads):
+      super(Block, self).__init__()
+      self.layer_norm = torch.nn.LayerNorm(embedding_dimensions)
+      self.attn = MultiHeadAttention(max_seq_len, embedding_dimensions, num_heads)
+      self.attn_add_and_norm = AddAndNorm(embedding_dimensions)
+      self.feed_forward = FeedForward(embedding_dimensions)
+      self.feed_forward_add_and_norm = AddAndNorm(embedding_dimensions)
 
+    def forward(self, x):
+      residual_x = x
+      output = self.layer_norm(x)
+      output = self.attn(output)
+      residual_ff_x = self.attn_add_and_norm(output, residual_x)
+      output = self.feed_forward(residual_ff_x)
+      output = self.feed_forward_add_and_norm(output, residual_ff_x)
+      return output
 
 class SelfAttention(torch.nn.Module):
     def __init__(self, max_seq_len, embedding_dimensions):
